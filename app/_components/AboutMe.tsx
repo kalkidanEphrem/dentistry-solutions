46465203fec30c4d7ac3c27432eb9b71b402dcfc
joephy527 <joephy527@gmail.com>
2024-01-11 09:36:07 -0800

    refactor code and put decorative svgs into separate files and import them as images

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -5,23 +5,18 @@ export default function About() {
     <section className="w-full flex flex-col-reverse lg:flex-row lg:justify-between items-center 2xl:px-24 xl:px-16 md:px-12 px-10 lg:py-32 xl:pb-28 py-10">
       <div className="mt-10 lg:mt-0 flex flex-col self-start 2xl:self-center lg:w-1/2 justify-start p-6">
         {/* icon above heading */}
-        <svg
-          width="56"
-          height="25"
-          viewBox="0 0 46 23"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+        <Image
+          src="/icons/heading.svg"
+          width={56}
+          height={25}
+          alt="heading icon"
           className="sm:ml-10"
-        >
-          <path
-            d="M41 23C41 18.2261 39.1036 13.6477 35.7279 10.2721C32.3523 6.89642 27.7739 5 23 5C18.2261 5 13.6477 6.89642 10.2721 10.2721C6.89642 13.6477 5 18.2261 5 23"
-            stroke="#00E5CC"
-            stroke-width="10"
-          />
-        </svg>
+        />
+
         <h2 className="text-primary font-bold text-3xl sm:text-4xl">
           About Me
         </h2>
+
         <p className="mt-3 mb-5 font-semibold text-justify text-xl text-card-paragraph">
           Dr Shenilee Hazell Graduated from the University of Bristol in 2007.
           After graduating from Bristol, she settled in the City of London where
