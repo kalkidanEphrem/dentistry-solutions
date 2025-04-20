b7392a86006b901ba0e268636ee4b484ff6586ec
Brook  Worku Alemu <109297876+BrookW-83@users.noreply.github.com>
2025-04-20 11:08:24 +0300

    refactor: Improve styling and structure of Signup, Hero, SignupForm, and AboutMe components

--- app/_components/home/Signup.tsx
+++ app/_components/home/Signup.tsx
@@ -3,33 +3,14 @@ import SignupForm from "./SignupForm";
 
 const Signup = () => {
   return (
-    <section className="w-full xl:px-28 xl:py-36 md:px-20 md:py-24 px-10 py-16 overflow-clip">
-      <div className="bg-tertiary p-6 md:p-8 lg:px-16 lg:py-8 rounded-xl relative">
-        {/* decorative svg */}
-        <Image
-          src="/icons/signup.svg"
-          width={510}
-          height={350}
-          alt="About me icon"
-          className="hidden lg:block absolute 2xl:right-0 2xl:top-16 xl:-right-[70px] lg:-right-32 lg:-top-20"
-        />
+    <section className="flex justify-center  items-center sm:mb-12 md:mb-24 min-h-screen shad  w-full xl:px-28 xl:py-36 md:px-20 md:py-24 px-10 py-16 overflow-clip">
+      <div className="p-6 md:p-8 lg:px-16 border border-primary lg:py-8 rounded-xl relative w-full max-w-4xl">
+        <div className="w-full flex flex-col items-center gap-10 z-10">
+          <h2 className="font-staatliches font-bold text-3xl sm:text-4xl">
+            Schedule Appointment
+          </h2>
 
-        <div className="w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2 flex flex-col gap-10 justify-between z-10">
-          <div className="flex flex-col sm:w-max">
-            {/* icon above heading */}
-            <Image
-              src="/icons/heading.svg"
-              width={56}
-              height={25}
-              alt="heading icon"
-              className="sm:self-end mr-2"
-            />
-
-            <h2 className="text-primary font-bold text-3xl sm:text-4xl">
-              Schedule Appointment
-            </h2>
-          </div>
-          <p className="text-primary text-justify mt-3 mb-5 font-semibold text-xl">
+          <p className="font-inter text-primary mt-3 mb-5 text-xl">
             Your Perfect Smile Awaits: Schedule Your Dental Care Appointment
             Today for a Lifetime of Radiant, Healthy Teeth and Unmatched Oral
             Wellness!
